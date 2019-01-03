a6ce68ceb4beed39312807b04a29aebcb8c31521
Artur Trzop <arturtrzop@gmail.com>
2019-01-03 19:29:23 +0100

    Use KNAPSACK_PRO_TEST_SUITE_TOKEN_JEST

--- src/env-config.ts
+++ src/env-config.ts
@@ -1,8 +1,8 @@
 export class EnvConfig {
   public static loadEnvironmentVariables(): void {
-    if (process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN_CYPRESS) {
+    if (process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN_JEST) {
       process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN =
-        process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN_CYPRESS;
+        process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN_JEST;
     }
   }
 
