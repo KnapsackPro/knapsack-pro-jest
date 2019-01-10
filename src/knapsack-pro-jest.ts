2f982619b29dbf824ef28ff2b739b3e1de98d03c
Artur Trzop <arturtrzop@gmail.com>
2019-01-10 15:46:56 +0100

    Use default time 0.1s when test file time equal zero. This way we meet Knapsack Pro API requirement to post only time execution greater than zero.
    Related:
    * https://github.com/KnapsackPro/knapsack_pro-ruby/pull/71
    * https://github.com/KnapsackPro/knapsack-pro-core-js/issues/3

--- src/knapsack-pro-jest.ts
+++ src/knapsack-pro-jest.ts
@@ -45,10 +45,14 @@ const onSuccess: onQueueSuccessType = async (queueTestFiles: TestFile[]) => {
         process.platform === 'win32'
           ? testFilePath.replace(`${projectPath}\\`, '').replace(/\\/g, '/')
           : testFilePath.replace(`${projectPath}/`, '');
+      const timeExecutionMiliseconds = end - start;
+      // 0.1s default time when not recorded timing
+      const timeExecution =
+        timeExecutionMiliseconds > 0 ? timeExecutionMiliseconds / 1000 : 0.1;
 
       return {
         path,
-        time_execution: (end - start) / 1000,
+        time_execution: timeExecution,
       };
     },
   );
